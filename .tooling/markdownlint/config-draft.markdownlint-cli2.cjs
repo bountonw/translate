efcf448e5dd4ad586910106e1fe844c3b2b62fd4
Matthew Leffler <mattleff@gmail.com>
2024-01-30 11:39:59 +0700

    Add empty MB files for Lo (#51)
    Fixes #50

--- .tooling/markdownlint/config-draft.markdownlint-cli2.cjs
+++ .tooling/markdownlint/config-draft.markdownlint-cli2.cjs
@@ -3,6 +3,7 @@ module.exports = {
     "fenced-code-language": false,
     "line-length": false,
     "no-bare-urls": false,
+    "no-duplicate-heading": false,
     "no-inline-html": false,
     "single-trailing-newline": false,
     "heading-style": {
