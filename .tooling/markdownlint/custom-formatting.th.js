0fe04834517a42d689106a4380df27de32a0c8cb
Matthew Leffler <mattleff@gmail.com>
2024-02-18 12:25:53 +0700

    Set @bountonw as code owner for forbidden terms

--- .tooling/markdownlint/custom-formatting.th.js
+++ .tooling/markdownlint/custom-formatting.th.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 const forbiddenTerms = fs
   .readFileSync(
-    path.resolve(".tooling/markdownlint", "./forbidden_thai_terms.txt"),
+    path.resolve(".tooling/markdownlint", "./forbidden_terms/thai.txt"),
     "utf-8"
   )
   .split("\n")
