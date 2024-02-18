beaed84faa2705c42251d9580ca8f4c6b290bcdf
Brian Wilson <15809934+bountonw@users.noreply.github.com>
2024-02-18 11:26:18 +0700

    Add forbidden_lao_terms.txt file (#119)
    List of common typos and misspellings of Lao words to be flagged in
    linting Lao `*.md` files.
    
    * Rename `forbidden-terms.md` to `forbidden_thai_terms.txt. 
    * Update linting calls to include both files.
    * Fix spelling errors flagged by new linting rules.
    
    ---------
    
    Co-authored-by: Matthew Leffler <mattleff@gmail.com>

---
+++ .tooling/markdownlint/custom-formatting.js
@@ -0,0 +1,56 @@
+import { forEachLine, getLineMetadata } from "markdownlint-rule-helpers";
+
+const customFormattingRules = [
+  {
+    name: "invalid spacing",
+    regexp: /\S\s{2,}($|\S)/,
+  },
+  {
+    name: "no space before '{'",
+    regexp: /\)[^ ’”;:,.!\[]/,
+  },
+  {
+    name: "closing parenthesis not properly followed",
+    regexp: /\)[^ ’”;:,.!\[]/,
+  },
+  {
+    name: "missing space after closing curly quotes",
+    regexp: /[’”][^\s<’”—\.\[]/,
+  },
+];
+
+const clamp = (number, min, max) => {
+  return Math.max(min, Math.min(number, max));
+};
+
+const extractContext = (line, column) => {
+  const contextPadding = 10;
+  return line.substr(
+    clamp(column - contextPadding, 0, line.length - 1),
+    contextPadding * 2
+  );
+};
+
+const CustomFormatting = {
+  names: ["custom-formatting"],
+  description: "Custom formatting rules",
+  tags: ["style"],
+  function: (params, onError) => {
+    forEachLine(getLineMetadata(params), (line, lineIndex) => {
+      for (const rule of customFormattingRules) {
+        const { name, regexp } = rule;
+        const column = line.search(regexp);
+        if (column >= 0) {
+          onError({
+            lineNumber: lineIndex + 1,
+            detail: `Found ${name} at column ${column}`,
+            context: extractContext(line, column),
+            range: [column + 1, 1],
+          });
+        }
+      }
+    });
+  },
+};
+
+export default CustomFormatting;
