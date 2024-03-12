e19b33f182de067f85675afc34e73a8ca1283e1b
Matthew Leffler <mattleff@gmail.com>
2024-03-12 11:37:54 +0700

    Apply forbidden term checks to all translation stages (#170)
    Previously Thai forbidden terms were not being checked until public; now
    they will be checked at all translation stages. This PR also DRYs out
    some of the duplicated code across the various custom validation code.
    
    Thai translation files in the raw and edit folders have been updated to pass linting. A few additional forbidden terms were also added to the Thai forbidden terms file. 
    ---------
    
    Co-authored-by: Brian Wilson <15809934+bountonw@users.noreply.github.com>

--- .tooling/markdownlint/custom-formatting.js
+++ .tooling/markdownlint/custom-formatting.js
@@ -1,4 +1,4 @@
-import { forEachLine, getLineMetadata } from "markdownlint-rule-helpers";
+import { applyCustomFormattingRules } from "./helpers.js";
 
 const customFormattingRules = [
   {
@@ -19,38 +19,11 @@ const customFormattingRules = [
   },
 ];
 
-const clamp = (number, min, max) => {
-  return Math.max(min, Math.min(number, max));
-};
-
-const extractContext = (line, column) => {
-  const contextPadding = 10;
-  return line.substr(
-    clamp(column - contextPadding, 0, line.length - 1),
-    contextPadding * 2
-  );
-};
-
 const CustomFormatting = {
   names: ["custom-formatting"],
   description: "Custom formatting rules",
   tags: ["style"],
-  function: (params, onError) => {
-    forEachLine(getLineMetadata(params), (line, lineIndex) => {
-      for (const rule of customFormattingRules) {
-        const { name, regexp } = rule;
-        const column = line.search(regexp);
-        if (column >= 0) {
-          onError({
-            lineNumber: lineIndex + 1,
-            detail: `Found ${name} at column ${column}`,
-            context: extractContext(line, column),
-            range: [column + 1, 1],
-          });
-        }
-      }
-    });
-  },
+  function: applyCustomFormattingRules(customFormattingRules),
 };
 
 export default CustomFormatting;
--- .tooling/markdownlint/custom-formatting.lo.js
+++ .tooling/markdownlint/custom-formatting.lo.js
@@ -1,53 +1,17 @@
-import { forEachLine, getLineMetadata } from "markdownlint-rule-helpers";
-import fs from "fs";
-import path from "path";
-const forbiddenTerms = fs
-  .readFileSync(
-    path.resolve(".tooling/markdownlint", "./forbidden_terms/lao.txt"),
-    "utf-8"
-  )
-  .split("\n")
-  .filter((l) => l && l.charAt(0) !== "#");
+import { applyCustomFormattingRules, readForbiddenTerms } from "./helpers.js";
 
 const customFormattingRules = [
   {
     name: "forbidden term",
-    regexp: new RegExp(`${forbiddenTerms.join("|")}`),
+    regexp: new RegExp(`${readForbiddenTerms("lao.txt").join("|")}`),
   },
 ];
-
-const clamp = (number, min, max) => {
-  return Math.max(min, Math.min(number, max));
-};
-
-const extractContext = (line, column) => {
-  const contextPadding = 10;
-  return line.substr(
-    clamp(column - contextPadding, 0, line.length - 1),
-    contextPadding * 2
-  );
-};
 
 const CustomFormatting = {
   names: ["custom-formatting-lo"],
   description: "Custom formatting (lo) rules",
   tags: ["style"],
-  function: (params, onError) => {
-    forEachLine(getLineMetadata(params), (line, lineIndex) => {
-      for (const rule of customFormattingRules) {
-        const { name, regexp } = rule;
-        const column = line.search(regexp);
-        if (column >= 0) {
-          onError({
-            lineNumber: lineIndex + 1,
-            detail: `Found ${name} at column ${column}`,
-            context: extractContext(line, column),
-            range: [column + 1, 1],
-          });
-        }
-      }
-    });
-  },
+  function: applyCustomFormattingRules(customFormattingRules),
 };
 
 export default CustomFormatting;
