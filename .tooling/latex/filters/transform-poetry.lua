9f29dc211b39c7afd31c1e890dcf6862412a8029
Matthew Leffler <mattleff@gmail.com>
2024-03-12 17:30:14 +0700

    Switch to Line Block

--- .tooling/latex/filters/transform-poetry.lua
+++ .tooling/latex/filters/transform-poetry.lua
@@ -1,8 +1,8 @@
 -- local logging = require 'logging'
 
 function CodeBlock (block)
-    -- logging.temp('pandoc', block)
-    return pandoc.BlockQuote(block.text)
+    -- logging.temp('pandoc', pandoc.read('| ' .. block.text:gsub('\n', '\n| '), 'markdown').blocks[1])
+    return pandoc.read('| ' .. block.text:gsub('\n', '\n| '), 'markdown').blocks[1]
   end
 
 -- TODO: transform quote indentation to nested quotes?
