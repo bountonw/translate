06597880a638920f5b6a36c2f52f1b966a1585be
Matthew Leffler <mattleff@gmail.com>
2024-10-09 17:27:57 +0700

    First pass at static site

---
+++ site/pages/_meta.js
@@ -0,0 +1,26 @@
+// TODO: generate this in build.js
+export default {
+  index: "หน้าแรก",
+  LBF: "อยู่ด้วยความเชื่อ",
+  MB: "ปัญญาจากคำเทศนาบนภูเขา",
+  PP: "บรรพชนกับผู้เผยพระวจนะ",
+  "*": {
+    theme: {
+      typesetting: "article",
+      footer: {
+        text: (
+          <span>
+            This translation is licensed under a
+            <a
+              href="https://github.com/bountonw/translate/blob/main/LICENSE"
+              target="_blank"
+            >
+              Creative Commons Attribution-NoDerivs 4.0 International License
+            </a>
+            .
+          </span>
+        ),
+      },
+    },
+  },
+};
