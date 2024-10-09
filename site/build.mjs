baea597c92fe2567df97660671fd8f530b862813
Matthew Leffler <mattleff@gmail.com>
2024-10-09 17:27:57 +0700

    First pass at static site

---
+++ site/build.mjs
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const DOCS_PATH = "../th";
+const PAGES_PATH = "./pages";
+
+const getFolders = () => {
+  const folders = fs
+    .readdirSync(DOCS_PATH, { recursive: true })
+    .filter((file) => {
+      const stat = fs.statSync(path.join(DOCS_PATH, file));
+      return stat && stat.isDirectory() && path.basename(file) === "03_public";
+    });
+  return folders;
+};
+
+const processFolder = (folder) => {
+  const destinationDir = path.join(PAGES_PATH, path.dirname(folder));
+  fs.mkdirSync(destinationDir, { recursive: true });
+
+  // copy files
+  const sourceDir = path.join(DOCS_PATH, folder);
+  copyFiles(sourceDir, destinationDir);
+};
+
+const copyFiles = (sourceDir, destinationDir) => {
+  fs.readdirSync(sourceDir, { recursive: true })
+    .filter((file) => {
+      const stat = fs.statSync(path.join(sourceDir, file));
+      return stat && stat.isFile() && path.extname(file) === ".md";
+    })
+    .map((file) => {
+      // This is only necessary because the filename will become the final URL slug
+      const newFilename = file.replace(/[A-Z]+(.*)_th.md/, "$1.md");
+      copyFile(
+        path.join(sourceDir, file),
+        path.join(destinationDir, newFilename)
+      );
+    });
+};
+
+const copyFile = (fromPath, toPath) => {
+  const source = fs.readFileSync(fromPath, "utf-8");
+  const matterObject = matter(source);
+  const title = `${matterObject.data.chapter.number}. ${matterObject.data.chapter.title.th}`;
+  const metadata = {
+    ...matterObject.data,
+    title,
+  };
+  const content = `# ${title}
+
+${matterObject.content.replace(/\n##\s+[^}]+\}/g, "")}`;
+  console.log(metadata);
+  fs.writeFileSync(toPath, matter.stringify(content, metadata));
+
+  // remove h3s?
+};
+
+// Run!
+getFolders().map(processFolder);
---
+++ site/theme.config.jsx
@@ -0,0 +1,24 @@
+export default {
+  logo: <span>เอลเลน จี ไวท์</span>,
+  project: {
+    link: "https://github.com/bountonw/translate",
+  },
+  editLink: {
+    component: null
+  },
+  feedback: {
+    content: null
+  },
+  search: false,
+  gitTimestamp: timestamp => <div>Testing the time: {timestamp}</div>, // TODO: not working
+  footer: {
+    content: (
+      <span>
+        This translation is licensed under a{' '}
+        <a href="https://github.com/bountonw/translate/blob/main/LICENSE" target="_blank">Creative Commons Attribution-NoDerivs 4.0 International License</a>.
+      </span>
+    )
+  }
+};
+
+// noindex
