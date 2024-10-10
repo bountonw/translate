a9d0fa97cee5da1201a94984db7562a4d5c441ec
Matthew Leffler <mattleff@gmail.com>
2024-10-10 20:50:12 +0700

    Some tweaks

--- site/pages/_meta.js
+++ site/pages/_meta.js
@@ -7,20 +7,7 @@ export default {
   "*": {
     theme: {
       typesetting: "article",
-      footer: {
-        text: (
-          <span>
-            This translation is licensed under a
-            <a
-              href="https://github.com/bountonw/translate/blob/main/LICENSE"
-              target="_blank"
-            >
-              Creative Commons Attribution-NoDerivs 4.0 International License
-            </a>
-            .
-          </span>
-        ),
-      },
+      timestamp: false,
     },
   },
 };
