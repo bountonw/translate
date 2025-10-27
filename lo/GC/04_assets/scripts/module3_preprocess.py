e51e4d0da4d25d80483f4bb803100b7d672a4f0a
Michael Babienco <mbabienco@southern.edu>
2025-10-27 10:26:00 +0900

    Add stubs for dedication, intro pages

--- lo/GC/04_assets/scripts/module3_preprocess.py
+++ lo/GC/04_assets/scripts/module3_preprocess.py
@@ -114,18 +114,24 @@ def create_tex_file(tex_input_paths, output_dir, tex_scripts_path, debug=False):
     # Build .tex
     tex_content = []
 
-    # 1) Docclass
+    # Docclass
     tex_content.append("% Document class and basic setup")
     tex_content.append(f"\\input{{{tex_scripts_norm}/docclass}}")
     tex_content.append("")
 
-    # 2) Header
-
-    # 3) Document + body
+    # Document + body
     tex_content.append("\\begin{document}")
     tex_content.append("")
     # Page 1: absolutely empty header/footer
     tex_content.append("\\thispagestyle{empty}")
+
+    # add title and intro page (if tex_input_paths > 1)
+    if len(tex_input_paths) > 1:
+        tex_content.append(f"\\frontmatter")
+        tex_content.append(f"\\input{{{tex_scripts_norm}/title_page}}")
+        tex_content.append(f"\\input{{{tex_scripts_norm}/dedication_page}}")
+        tex_content.append("\\cleardoublepage")
+
     # Folios: plain footer, arabic, start at 1
     tex_content.append("\\GCInitFolios")
     tex_content.append("")
@@ -134,7 +140,9 @@ def create_tex_file(tex_input_paths, output_dir, tex_scripts_path, debug=False):
         tex_content.append("\\tableofcontents")
         tex_content.append("\\cleardoublepage")
         tex_content.append("")
+        tex_content.append(f"\\input{{{tex_scripts_norm}/intro_chapter}}")
 
+    tex_content.append(f"\\mainmatter")
     tex_content.append("% Header commands and formatting")
     tex_content.append(f"\\input{{{tex_scripts_norm}/tex_header_info}}")
     tex_content.append("")
